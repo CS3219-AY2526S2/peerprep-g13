736a07a863cb87a34feea9cc686e633d9375d466
Carameow <iywudla@gmail.com>
2026-03-15 13:02:42 +0800

    Add exceptions and their handlers

---
+++ server/src/main/java/com/g13cs3219/server/exceptions/EmptyArgumentException.java
@@ -0,0 +1,7 @@
+package com.g13cs3219.server.exceptions;
+
+public class EmptyArgumentException extends RuntimeException {
+    public EmptyArgumentException() {
+        super("Email and password cannot be empty");
+    }
+}
---
+++ server/src/main/java/com/g13cs3219/server/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.g13cs3219.server.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException() {
+        super("Invalid email or password");
+    }
+}
