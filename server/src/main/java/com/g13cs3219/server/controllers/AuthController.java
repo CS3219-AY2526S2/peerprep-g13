f4507affd9202a2b3d91b80280c708aa7e68ed7a
Carameow <iywudla@gmail.com>
2026-03-15 15:21:02 +0800

    Change /users/auth path to /user/auth

--- server/src/main/java/com/g13cs3219/server/controllers/AuthController.java
+++ server/src/main/java/com/g13cs3219/server/controllers/AuthController.java
@@ -14,7 +14,7 @@ import com.g13cs3219.server.services.AuthService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/users/auth")
+@RequestMapping("/user/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
