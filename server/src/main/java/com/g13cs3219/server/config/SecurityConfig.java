1ef40da7e6d012fa661ab331169abaea5bef2bea
Carameow <iywudla@gmail.com>
2026-03-17 16:17:37 +0800

    Add missing filter from roleConfig

--- server/src/main/java/com/g13cs3219/server/config/SecurityConfig.java
+++ server/src/main/java/com/g13cs3219/server/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -60,6 +61,8 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.PUT, "/user/*/role", "/questions/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/questions").hasRole("ADMIN")
