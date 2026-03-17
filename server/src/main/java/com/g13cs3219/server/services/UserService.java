43d55418b364c7bd0aa956a82b30608bdc7d79a8
Carameow <iywudla@gmail.com>
2026-03-17 14:57:31 +0800

    Validate password and email

--- server/src/main/java/com/g13cs3219/server/services/UserService.java
+++ server/src/main/java/com/g13cs3219/server/services/UserService.java
@@ -1,11 +1,13 @@
 package com.g13cs3219.server.services;
 
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.g13cs3219.server.dto.requests.UpdateRoleRequest;
 import com.g13cs3219.server.dto.responses.UpdateRoleResponse;
 import com.g13cs3219.server.exceptions.EmailAlreadyExistsException;
+import com.g13cs3219.server.exceptions.InvalidEmailFormatException;
 import com.g13cs3219.server.exceptions.UserNotFoundException;
 import com.g13cs3219.server.model.Role;
 import com.g13cs3219.server.model.User;
@@ -29,24 +31,35 @@ public class UserService {
      * @return a response containing the new role of the target user
      */
     @Transactional
-    public UpdateRoleResponse updateRole(Long targetId, UpdateRoleRequest request) {
+    public UpdateRoleResponse updateRole(Long targetId, Authentication authentication, UpdateRoleRequest request) {
         // Validate the request
         UpdateRoleRequest.validateRequest(request);
 
         // Get the admin and target user
-        Long adminId = request.getAdminId();
-        User admin = getUserById(adminId);
+        User admin = (User) authentication.getPrincipal();
         User target = getUserById(targetId);
-
-        // Verify admin
         verifyAdmin(admin);
-        passwordService.verifyPassword(admin, request.getPassword());
 
         // Update the user's role
         Role role = Role.fromId(request.getNewRole());
         userRepository.updateUserRole(target, role);
 
         return UpdateRoleResponse.buildResponse(role);
+    }
+
+    /**
+     * Validates the format of the given email. The email should be in the format of 'youremail@gmail.com'.
+     * @param email the email to validate
+     * @throws IllegalArgumentException    if the email is null or blank
+     * @throws InvalidEmailFormatException if the email does not match the required format
+     */
+    public void validateEmail(String email) {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("Email cannot be null or blank.");
+        }
+        if (!email.matches("^[A-Za-z0-9+_.-]+@gmail\\.com$")) {
+            throw new InvalidEmailFormatException();
+        }
     }
 
     /**
@@ -91,7 +104,7 @@ public class UserService {
      * @param email the email to check for existence
      * @throws EmailAlreadyExistsException if no user with the given email exists
      */
-    public void checkUserExistsByEmail(String email) {
+    public void checkEmailAlreadyExist(String email) {
         if (userRepository.existsByEmail(email)) {
             throw new EmailAlreadyExistsException(email);
         }
