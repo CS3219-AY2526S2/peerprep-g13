b6dc2d775ba57e50469a7c2da739bc8f8d6db515
Duong Huy Phuoc Chu <104732864+fuocchu@users.noreply.github.com>
2026-04-04 00:02:59 +0800

    ci: add GitHub Actions CI with GHCR push

---
+++ question-service/src/test/java/com/g13cs3219/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.g13cs3219.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class QuestionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
