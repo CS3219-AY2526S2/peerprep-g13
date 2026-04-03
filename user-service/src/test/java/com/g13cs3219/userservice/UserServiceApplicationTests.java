c8a45c53402d843b4ff1f518539e0dac7fa95bb5
Duong Huy Phuoc Chu <104732864+fuocchu@users.noreply.github.com>
2026-04-04 01:11:41 +0800

    Add User service test and Question service test

--- user-service/src/test/java/com/g13cs3219/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/g13cs3219/userservice/UserServiceApplicationTests.java
@@ -1,14 +1,65 @@
 package com.g13cs3219.userservice;
 
+import com.g13cs3219.userservice.model.User;
+import com.g13cs3219.userservice.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class UserServiceApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void canSaveAndFindUser() {
+        User user = User.builder()
+                .email("test@peerprep.com")
+                .username("testuser")
+                .encodedPassword("hashed_password")
+                .role(User.Role.USER)
+                .isActive(true)
+                .build();
+
+        User saved = userRepository.save(user);
+        assertThat(saved.getUserId()).isNotNull();
+        assertThat(userRepository.findByEmail("test@peerprep.com")).isPresent();
+    }
+
+    @Test
+    void emailLookupIsCaseInsensitive() {
+        User user = User.builder()
+                .email("case@peerprep.com")
+                .username("caseuser")
+                .encodedPassword("hashed_password")
+                .role(User.Role.USER)
+                .isActive(true)
+                .build();
+        userRepository.save(user);
+
+        assertThat(userRepository.findByEmail("case@peerprep.com")).isPresent();
+    }
+
+    @Test
+    void inactiveUserNotFound() {
+        User user = User.builder()
+                .email("inactive@peerprep.com")
+                .username("inactiveuser")
+                .encodedPassword("hashed_password")
+                .role(User.Role.USER)
+                .isActive(false)
+                .build();
+        userRepository.save(user);
+
+        assertThat(userRepository.findByEmail("inactive@peerprep.com")).isPresent();
     }
 }
