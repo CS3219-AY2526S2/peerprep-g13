34ef94e479c978cb6113d573f9f83d96ca6b47e1
Carameow <iywudla@gmail.com>
2026-04-04 11:54:05 +0800

    Remove unused imports

--- matching-service/src/main/java/com/g13cs3219/matching_service/services/ProducerService.java
+++ matching-service/src/main/java/com/g13cs3219/matching_service/services/ProducerService.java
@@ -1,7 +1,5 @@
 package com.g13cs3219.matching_service.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
