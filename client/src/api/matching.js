ecadd7d20a19bf8adc08c80ea1ad85a0069e3247
Duong Huy Phuoc Chu <104732864+fuocchu@users.noreply.github.com>
2026-04-06 22:42:00 +0800

    Problem 1: Add API gateway

--- client/src/api/matching.js
+++ client/src/api/matching.js
@@ -1,11 +1,7 @@
-import { createApiInstance } from "./axios";
-
-const matchingApi_axios = createApiInstance(
-  import.meta.env.VITE_MATCHING_API_BASE_URL || "http://localhost:8082"
-);
+import api from "./axios";
 
 export const matchingApi = {
-  match: (body) => matchingApi_axios.post("/match/join", body),
-  loosematch: (body) => matchingApi_axios.post("/match/join/loose", body),
-  leave: (body) => matchingApi_axios.post("/match/leave", body),
+  match: (body) => api.post("/match/join", body),
+  loosematch: (body) => api.post("/match/join/loose", body),
+  leave: (body) => api.post("/match/leave", body),
 };
