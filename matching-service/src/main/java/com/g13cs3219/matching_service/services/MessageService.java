6dab5ebcfabbb08eb76e0e639f201569072fa1af
Carameow <iywudla@gmail.com>
2026-04-07 03:13:02 +0800

    Call api get question internally in backend

--- matching-service/src/main/java/com/g13cs3219/matching_service/services/MessageService.java
+++ matching-service/src/main/java/com/g13cs3219/matching_service/services/MessageService.java
@@ -2,19 +2,34 @@ package com.g13cs3219.matching_service.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.g13cs3219.matching_service.dto.requests.QuestionRequest;
 import com.g13cs3219.matching_service.dto.responses.MatchResult;
+import com.g13cs3219.matching_service.dto.responses.QuestionResponse;
+import com.g13cs3219.matching_service.dto.responses.QuestionResponseWrapper;
 
 import lombok.AllArgsConstructor;
 
 @Service
-@AllArgsConstructor
 public class MessageService {
 
+    @Value("${question-service.url}")
+    private String questionServiceURL;
+
     private static final Logger log = LoggerFactory.getLogger(MessageService.class.getName());
+
     private final SimpMessagingTemplate messagingTemplate;
+    private final WebClient.Builder webClientBuilder;
+
+    public MessageService(SimpMessagingTemplate messagingTemplate, WebClient.Builder webClientBuilder) {
+        this.messagingTemplate = messagingTemplate;
+        this.webClientBuilder = webClientBuilder;
+    }
 
     /**
      * Send a timeout message to the user when they have been waiting for too long.
@@ -37,6 +52,13 @@ public class MessageService {
      */
     public void sendMatchFoundMessage(MatchResult match) {
         log.info("Sending match found message to users: {} and {}", match.getUserId1(), match.getUserId2());
+
+        QuestionRequest request = QuestionRequest.buildQuestionRequest(match.getTopic(), match.getDifficulty());
+        Long randomQuestionId = getRandomQuestion(request).getQuestion().getQuestionId();
+
+        match.setQuestionId(randomQuestionId);
+        log.info("Random question found: {}", randomQuestionId);
+
         messagingTemplate.convertAndSendToUser(
             match.getUserId1() + "",
             "/queue/match",
@@ -61,5 +83,17 @@ public class MessageService {
             "/queue/match",
             "Match cancelled"
         );
+    }
+
+    private QuestionResponseWrapper getRandomQuestion(QuestionRequest request) {
+        return webClientBuilder
+                .build()
+                .post()
+                .uri(questionServiceURL + "/questions/match")
+                .header("X-Internal-Request", "true")
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(QuestionResponseWrapper.class)
+                .block();
     }
 }
